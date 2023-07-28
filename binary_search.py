5df2eb768972c9fe8db3ed6ce50bfb1ba815d6c8
brockmeyer7 <brockmeyer05@gmail.com>
2023-07-28 06:07:51 -0500

    binary_search

--- binary_search.py
+++ binary_search.py
@@ -1,13 +1,25 @@
 def search(nums: list, target: int) -> int:
-    start = 0
-    end = len(nums) // 2
-    while start != end:
-        if nums[start] == target:
-            return start
-        elif nums[end] == target:
-            return end
-        elif nums[start] < target < nums[end]:
-            nums = nums[: end + 1]
-            end = len(nums) // 2
+    idx = 0
+    mid = len(nums) // 2
+    if target > nums[-1] or target < nums[0]:
+        return -1
+    while len(nums) > 1:
+        if target < nums[0]:
+            return -1
+        elif nums[0] <= target < nums[mid]:
+            nums = nums[: mid]
+            mid = len(nums) // 2
         else:
-            nums = nums[end + 1]
+            idx += mid
+            nums = nums[mid:]
+            mid = len(nums) // 2
+    if nums[0] == target:
+        return idx
+    return -1
+
+
+if __name__ == '__main__':
+    tc = [[[-1, 0, 3, 5, 9, 12], 9], [[-1, 0, 3, 5, 9, 12], 2], [[2, 5], 5]]
+
+    for t in tc:
+        print(t[0], t[1], search(t[0], t[1]))
